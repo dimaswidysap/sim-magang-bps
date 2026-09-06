a0dfae5457517a8bb557c966a33450294fdf5a8a
dimaswidysap <dimaswidysaputra41gmail.com>
2026-09-06 17:05:24 +0700

    'asasasas'

--- resources/views/components/BuldingBg.blade.php
+++ resources/views/components/BuldingBg.blade.php
@@ -1,4 +1,9 @@
-<img src="{{ asset('images/assets/gedung-bps.png') }}" alt="Gedung BPS"
-    class="absolute inset-0 h-full w-full object-cover" />
+{{-- Parent Container (Pastikan diberi overflow-hidden) --}}
+<div class="relative w-full h-full overflow-hidden">
 
-<div class="absolute inset-0 bg-linear-to-t from-black/80 via-black/40 to-black/20"></div>
+    <img src="{{ asset('images/assets/gedung-bps.png') }}" alt="Gedung BPS"
+        class="absolute inset-0 h-full w-full object-cover animate-zoom-infinite" />
+
+    <div class="absolute inset-0 bg-linear-to-t from-black/80 via-black/40 to-black/20"></div>
+
+</div>
